de3e5a75f048502e3830244c5c509b7b6870fb24
Tejas Sharma <sharma.tejas4776@gmail.com>
2026-07-26 14:20:45 +0000

    Added the basic read and write functions for the driver

--- msg-queue-driver/driver/driver.c
+++ msg-queue-driver/driver/driver.c
@@ -8,21 +8,53 @@ static dev_t dev_num;
 static struct class* cl;
 // static struct device msgqueue_1;
 static struct cdev msgqueue_cdev;
-
-
+static char queue_data[1024];
+static size_t data_occupied = 0;
 static ssize_t msgqueue_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos) {
     printk(KERN_INFO "Driver read called.\n");
-    return 0;
+    if (*f_pos > 0) {
+        return 0;
+    }
+    if (copy_to_user(buf, queue_data, data_occupied)) {
+        printk(KERN_ERR "Error in copying data from kernel to user. \n");
+        return -1;
+    }
+    *f_pos += data_occupied;
+    printk(KERN_INFO "Data copied successfully from kernel to user.\n");
+    return data_occupied;
 }
 
 static ssize_t msgqueue_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
     printk(KERN_INFO "Driver write called.\n");
+    if (len > sizeof(queue_data)) {
+        len = sizeof(queue_data) - 1;
+        printk(KERN_WARNING "Truncating the input as data is overloaded.\n");
+    }
+    data_occupied = len;
+    if (copy_from_user(queue_data, buf, len)) {
+        printk(KERN_ERR "Error in copying data from user to kernel.\n");
+        return -1;
+    }
+    queue_data[len] = '\0';
+    printk(KERN_INFO "Data copied successfully from user to kernel.");
+    return len;
+}
+
+static int msgqueue_open(struct inode* inode, struct file* filep) {
+    printk(KERN_INFO "Driver open called.\n");
+    return 0;
+}
+
+static int msgqueue_release(struct inode* inode, struct file* filep) {
+    printk(KERN_INFO "Driver release called.\n");
     return 0;
 }
 
 struct file_operations fops = {
     .read = msgqueue_read,
-    .write = msgqueue_write
+    .write = msgqueue_write,
+    .open = msgqueue_open,
+    .release = msgqueue_release
 };
 
 static int __init msgqueue_init(void) {
